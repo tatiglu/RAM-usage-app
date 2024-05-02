4cd4f96b4ff64a94ef90f4ef0700fe5efc64a2ed
Tatiana Glushenko <tati.glushenko97@gmail.com>
2024-05-02 10:29:36 +0200

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    while (true) {
+        system("cls");  
+        MEMORYSTATUSEX memInfo;
+        memInfo.dwLength = sizeof(MEMORYSTATUSEX);
+        GlobalMemoryStatusEx(&memInfo);
+
+        DWORDLONG totalPhysMem = memInfo.ullTotalPhys;
+        DWORDLONG physMemUsed = memInfo.ullTotalPhys - memInfo.ullAvailPhys;
+
+        std::cout << "Общий объем ОЗУ: " << totalPhysMem / 1024 / 1024 << " МБ" << std::endl;
+        std::cout << "Использовано ОЗУ: " << physMemUsed / 1024 / 1024 << " МБ" << std::endl;
+        std::cout << "Свободно ОЗУ: " << memInfo.ullAvailPhys / 1024 / 1024 << " МБ" << std::endl;
+
+        std::this_thread::sleep_for(std::chrono::seconds(5)); 
+    }
+
+    return 0;
+}
